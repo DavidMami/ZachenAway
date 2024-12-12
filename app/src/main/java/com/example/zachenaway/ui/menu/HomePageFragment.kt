3a47686e8db6b3380f22338bb620c894ea23595c
davidmami <xdavidx1000@gmail.com>
2024-12-12 23:56:08 +0200

    implement the home page logic

--- app/src/main/java/com/example/zachenaway/ui/menu/HomePageFragment.kt
+++ app/src/main/java/com/example/zachenaway/ui/menu/HomePageFragment.kt
@@ -1,4 +1,52 @@
 package com.example.zachenaway.ui.menu
 
-class HomePageFragment {
+import com.example.zachenaway.data.adapter.PostsListAdapter
+import com.example.zachenaway.data.database.schema.PostWithUser
+import com.example.zachenaway.databinding.FragmentHomePageBinding
+import com.example.zachenaway.viewmodel.HomePageViewModel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class HomePageFragment : Fragment() {
+
+    private var _binding: FragmentHomePageBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var homePageViewModel: HomePageViewModel
+    private lateinit var adapter: PostsListAdapter
+    private val posts = mutableListOf<PostWithUser>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        homePageViewModel = ViewModelProvider(this)[HomePageViewModel::class.java]
+        adapter = PostsListAdapter(layoutInflater, posts)
+        binding.PostsList.layoutManager = LinearLayoutManager(context)
+        binding.PostsList.adapter = adapter
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.PostsList.setHasFixedSize(false)
+        binding.homePageSpinner.show()
+
+        homePageViewModel.getAllPostsWithUser().observe(viewLifecycleOwner) { updatedPosts ->
+            binding.homePageSpinner.hide()
+            adapter.setPostsList(updatedPosts)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
